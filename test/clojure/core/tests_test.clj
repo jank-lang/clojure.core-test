3f1d4f0c09266d020c720dcd0f9978ea6be5dd3c
Olav Frengstad <olav@tiny-mesh.com>
2024-04-02 19:46:28 +0200

    move abs test to own file

--- test/clojure/core/tests_test.clj
+++ test/clojure/core/tests_test.clj
@@ -1,20 +1,6 @@
 (ns clojure.core.tests-test
   (:require [clojure.test :refer :all]))
 
-(deftest test-abs
-  (are [in ex] (= ex (abs in))
-    -1 1
-    1 1
-    Long/MIN_VALUE Long/MIN_VALUE ;; special case!
-    -1.0 1.0
-    -0.0 0.0
-    ##-Inf ##Inf
-    ##Inf ##Inf
-    -123.456M 123.456M
-    -123N 123N
-    -1/5 1/5)
-  (is (NaN? (abs ##NaN))))
-
 (deftest test-and
   ; (and) should return true
   (is true (and))
