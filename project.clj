dd453807abd324c97b5727e260d529140ff45987
jeaye <contact@jeaye.com>
2024-10-03 13:35:43 -0700

    Update project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure.core.tests "0.1.0-SNAPSHOT"
-  :description "Tests for clojure.core"
-  :url "https://github.com/lafka/clojure.core.tests"
+  :description "Dialect-independent tests for clojure.core, focused on locking down how Clojure JVM behaves so that other dialects to reach parity."
+  :url "https://github.com/jank-lang/clojure.core-test"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
